2a496f87a22d660d8a8d20d934d439cedc148939
wooki37 <lite1324@naver.com>
2023-06-21 09:31:14 +0900

    가까운 수:package:

---
+++ programers/introduction/PG062002.java
@@ -0,0 +1,23 @@
+
+// 코딩테스트 연습/코딩테스트 입문/가까운 수
+import java.util.ArrayList;
+import java.util.Collections;
+
+class Solution {
+  public int solution(int[] array, int n) {
+    int min = 100;
+    for (int i = 0; i < array.length; i++) {
+      if (Math.abs(array[i] - n) < min) {
+        min = Math.abs(array[i] - n);
+      }
+    }
+    ArrayList<Integer> arr = new ArrayList<Integer>();
+    for (int i = 0; i < array.length; i++) {
+      if ((Math.abs(array[i] - n)) == (min)) {
+        arr.add(array[i]);
+      }
+    }
+    Collections.sort(arr);
+    return arr.get(0);
+  }
+}
