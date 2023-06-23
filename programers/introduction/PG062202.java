b42a898a588ee4dd6e0f4fbddd58bd9690ff56e5
wooki37 <lite1324@naver.com>
2023-06-23 09:34:25 +0900

    특수문자 출력하기:package:

---
+++ programers/introduction/PG062202.java
@@ -0,0 +1,11 @@
+
+// 코딩테스트 연습/코딩 기초 트레이닝/특수문자 출력하기
+import java.util.Scanner;
+
+public class Solution {
+  public static void main(String[] args) {
+    System.out.println("!@#$%^&*(\\'\"<>?:;");
+  }
+}
+// 특수 문자열을 출력하기 위해서는 이스케이프 문자를 사용해야한다.
+// \를 사용하여 백슬래시()를 출력, '는 홑따옴표(')를 출력하기 위해 사용되며, \"는 쌍따옴표(")를 출력하기 위해 사용한다.
