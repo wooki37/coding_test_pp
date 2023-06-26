a02627241c553ef76b6d0ff2fa0c4e02ca8c77c7
wooki37 <lite1324@naver.com>
2023-06-27 00:46:11 +0900

    직사각형 얿이 구하기:package:

---
+++ programers/introduction/PG062602.java
@@ -0,0 +1,21 @@
+
+// 코딩테스트 연습/코딩테스트 입문/직사각형 넓이 구하기
+class Solution {
+  public int solution(int[][] dots) {
+
+    int answer = 0;
+    int minX = dots[0][0];
+    int minY = dots[0][1];
+    int maxX = dots[0][0];
+    int maxY = dots[0][1];
+
+    for (int i = 1; i < dots.length; i++) {
+      minX = Math.min(minX, dots[i][0]);
+      minY = Math.min(minY, dots[i][1]);
+      maxX = Math.max(maxX, dots[i][0]);
+      maxY = Math.max(maxY, dots[i][1]);
+    }
+    answer = (maxX - minX) * (maxY - minY);
+    return answer;
+  }
+}
