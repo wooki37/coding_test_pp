8e0b237fd733729ca958f10fda82cd90810036d2
wooki37 <lite1324@naver.com>
2023-06-30 00:21:48 +0900

    문자열 붙여서 출력하기:package:

---
+++ programers/introduction/PG062901.java
@@ -0,0 +1,13 @@
+
+// 코딩테스트 연습/코딩테스트 입문/문자열 붙여서 출력하기
+import java.util.Scanner;
+
+public class Solution {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    String a = sc.next();
+    String b = sc.next();
+
+    System.out.println(a + b);
+  }
+}
