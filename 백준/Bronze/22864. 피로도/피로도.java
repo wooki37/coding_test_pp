fb6eadbd24f7085f7445b368ba35eb442d645a7b
joohyunwook <133828393+wooki37@users.noreply.github.com>
2023-07-16 23:55:00 +0900

    [Bronze II] Title: 피로도, Time: 124 ms, Memory: 14124 KB -BaekjoonHub

--- 백준/Bronze/22864. 피로도/피로도.java
+++ 백준/Bronze/22864. 피로도/피로도.java
@@ -1,31 +1,32 @@
+
+// 22864 - 피로도
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    private void solution() throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int a = Integer.parseInt(st.nextToken());
-        int b = Integer.parseInt(st.nextToken());
-        int c = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
-        int sum = 0;
-        int aSum = 0;
-        for (int i = 0; i < 24; i++) {
-        	if (aSum+a <= m) {
-        		sum += b;
-        		aSum += a;
-        	} else {
-        		aSum -= c;
-        		if (aSum < 0)
-        			aSum = 0;
-        	}
-        }
-        System.out.print(sum);
-    }
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
 
-    public static void main(String[] args) throws Exception {
-        new Main().solution();
-    }
+		int A = Integer.parseInt(st.nextToken()); // 하루에 한시간 일해 쌓인 피로도
+		int B = Integer.parseInt(st.nextToken()); // 하루 한시간에 일한 양
+		int C = Integer.parseInt(st.nextToken()); // 한시간을 쉬면 줄어드는 피로도
+		int M = Integer.parseInt(st.nextToken()); // 최대한의 피로도(번아웃을 막기 위한 최대값)
+
+		int workedHour = 0; // 하루에 일한 양의 총합
+		int fatigue = 0; // 하루에 쌓인 피로도 총합
+		for (int i = 0; i < 24; i++) {
+			if (fatigue + A <= M) {
+				workedHour += B;
+				fatigue += A;
+			} else {
+				fatigue -= C;
+				if (fatigue < 0)
+					fatigue = 0;
+			}
+		}
+		System.out.print(workedHour);
+	}
 }
