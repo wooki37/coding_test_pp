517be1e9b1cc0891514e6b35e6c93c3b9a7eb84f
joohyunwook <133828393+wooki37@users.noreply.github.com>
2023-09-05 23:48:25 +0900

    [level 1] Title: 예산, Time: 0.35 ms, Memory: 75.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12982. 예산/예산.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[] d, int budget) {
+        int answer = 0;
+        
+        Arrays.sort(d);
+        
+        if (d[0] > budget) {
+            return 0;
+        }
+        for (int i = 0; i < d.length; i++) {
+            if (budget >= d[i]) {
+                budget -= d[i];
+                answer++;
+            }
+        }
+        
+        return answer;
+    }
+}
