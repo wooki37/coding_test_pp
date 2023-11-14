5650110689be52e9f659ba9104601ce39a6fb968
joohyunwook <133828393+wooki37@users.noreply.github.com>
2023-11-14 23:33:09 +0900

    [level 1] Title: 명예의 전당 (1), Time: 0.07 ms, Memory: 76.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Collections;
+
+class Solution {
+    public int[] solution(int k, int[] score) {
+        // 결과를 담을 배열 선언
+        int[] answer = new int[score.length];
+        
+        // 순위를 담을 List 생성
+        List<Integer> rank = new ArrayList<>();
+        
+        // score를 반복
+        for (int i = 0; i < score.length; i++) {
+            // score 추가
+            rank.add(score[i]);
+            
+            // rank 리스트의 크기가 k보다 크면 가장 낮은 점수 제거
+            if (rank.size() > k) {
+                rank.remove(Collections.min(rank));
+            }
+            // 남은 점수 중 가장 작은 점수 answer에 담기
+            answer[i] = Collections.min(rank);
+        }
+        return answer;
+    }
+}
