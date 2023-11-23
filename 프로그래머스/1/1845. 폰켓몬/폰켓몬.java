a6e9bbf283de62e2fd0034ede2328e15e502b1fe
joohyunwook <133828393+wooki37@users.noreply.github.com>
2023-11-23 23:16:55 +0900

    [level 1] Title: 폰켓몬, Time: 8.62 ms, Memory: 77.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -0,0 +1,17 @@
+import java.util.HashSet;
+
+class Solution {
+    public int solution(int[] nums) {
+        int answer = 0;
+        int pickNum = nums.length / 2;
+        
+        HashSet<Integer> set = new HashSet<>();
+        for (int n : nums) {
+            set.add(n);
+        }
+        if (pickNum >= set.size()) answer = set.size();
+        else answer = pickNum;
+        
+        return answer;
+    }
+}
